e8108df778719fde70b07e2506d997f901d455eb
GANDAPODI V SAATHVIK <113020084+SAATHVIK-GV@users.noreply.github.com>
2023-12-11 19:09:02 +0530

    Add files via upload

---
+++ calc.js
@@ -0,0 +1,15 @@
+function clearScreen() {
+        document.getElementById("screen").value = "";
+    }
+     
+    // This function display values
+    function displayscreen(value) {
+        document.getElementById("screen").value += value;
+    }
+     
+    // This function evaluates the expression and returns result
+    function calculate() {
+        var p = document.getElementById("screen").value;
+        var q = eval(p);
+        document.getElementById("screen").value = q;
+    }
